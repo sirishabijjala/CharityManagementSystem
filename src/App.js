d87051077b373b822a229269ad73600148e9fd82
sirishabijjala <sirishabijjala356@gmail.com>
2024-05-17 14:21:18 +0530

    charity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// Import React and required modules
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route, Link } from "react-router-dom";
 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Navbar from './Components/Navbar/Navbar';
+
+import Donate from './Components/Donate/Donate';
+import Profile from './Components/Profile/Profile';
+import View from './Components/View/View';
+import Access from './Components/Access/Access';
+import Signin from './Components/Signed/Signin';
+import Signup from './Components/Signed/Signup';
+import Contact from './Components/Navbar/Contact'
+import Home from './Components/Navbar/Home'
+
+
+// Functional component for the App
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  // JSX structure for the component
+  return (
+    <>
+   <Routes>
+                <Route exact path='/Donate' element={<Donate />}></Route>
+                <Route exact path='/View' element={<View />}></Route>
+                <Route exact path='/' element={<Signin />}></Route>
+                <Route exact path='/Profile' element={<Profile />}></Route>
+                <Route exact path='/Access' element={<Access />}></Route>
+                <Route exact path='/Signin' element={<Signin />}></Route>
+                <Route exact path='/Signup' element={<Signup />}></Route>
+                <Route exact path='/Navbar' element={<Navbar />}></Route>
+                <Route exact path='/Home' element={<Home />}></Route>
+                <Route exact path='/Contact' element={<Contact />}></Route>
+            </Routes>
+
+    </>
+  );
+};
+
+
+
+// Export the component
 export default App;
